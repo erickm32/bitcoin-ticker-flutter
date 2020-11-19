d466a2d1f2c3f9891ec97241f1f2147d73cfa6db
Érick Moreira <erick@sementesrastreadas.com.br>
2020-11-18 21:06:56 -0300

    Finishing project with a little extra. Had some trouble with Futures, but managed to get some sort of cache to avoid repeated API calls. The FutureBuilder, clearly, can be improved, but I'm done with this.

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,8 @@
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +33,51 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const COIN_API_KEY = 'EDA6AB7D-95F3-4E3E-9DE6-8DE4B73F8189';
+const BASE_COIN_API_URL = 'https://rest.coinapi.io/v1/exchangerate';
+
+class CoinData {
+  Map<String, Map<String, double>> currenciesData = {};
+
+  CoinData() {
+    for (String cryptoCurrency in cryptoList) {
+      currenciesData[cryptoCurrency] = {};
+    }
+  }
+
+  String _requestUrl(String cryptoCurrency, String currency) {
+    return '$BASE_COIN_API_URL/$cryptoCurrency/$currency?apikey=$COIN_API_KEY';
+  }
+
+  Future<void> _fetchData(String currency) async {
+    double exchangeRate = 0.0;
+    for (String cryptoCurrency in cryptoList) {
+      if (currenciesData[cryptoCurrency][currency] == null) {
+        http.Response response = await http.get(_requestUrl(cryptoCurrency, currency));
+        var fetchedData = await JsonDecoder().convert(response.body);
+        print(fetchedData);
+        if (fetchedData != null) {
+          exchangeRate = fetchedData['rate'].toDouble();
+        } else {
+          debugger(message: 'fetchedData is null');
+        }
+        currenciesData[cryptoCurrency][currency] = exchangeRate;
+      }
+    }
+    return;
+  }
+
+  Future<List<Map<String, double>>> getCoinDataFor(String currency) async {
+    List<Map<String, double>> data = [];
+
+    await _fetchData(currency);
+    for (String cryptoCurrency in cryptoList) {
+      print('on getCointDataFor');
+      print(currenciesData[cryptoCurrency][currency]);
+      var coinData = currenciesData[cryptoCurrency][currency];
+      data.add({cryptoCurrency: coinData != null ? coinData : 0.0});
+    }
+
+    return data;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -12,45 +12,110 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedValue = 'USD';
+  CoinData coinData = CoinData();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('🤑 Coin Ticker'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
+      body: FutureBuilder(
+        future: coinData.getCoinDataFor(selectedValue),
+        builder: (context, snapshot) {
+          List<Map<String, double>> exchangeData = [];
+          if (snapshot.hasData) {
+            exchangeData = snapshot.data;
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+                  child: Card(
+                    color: Colors.lightBlueAccent,
+                    elevation: 5.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                      child: Text(
+                        '1 ${exchangeData[0].keys.first} = ${exchangeData[0].values.first.toStringAsFixed(2)} $selectedValue',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+                  child: Card(
+                    color: Colors.lightBlueAccent,
+                    elevation: 5.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                      child: Text(
+                        '1 ${exchangeData[1].keys.first} = ${exchangeData[1].values.first.toStringAsFixed(2)} $selectedValue',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+                  child: Card(
+                    color: Colors.lightBlueAccent,
+                    elevation: 5.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                      child: Text(
+                        '1 ${exchangeData[2].keys.first} = ${exchangeData[2].values.first.toStringAsFixed(2)} $selectedValue',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 150.0,
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.only(bottom: 30.0),
+                  color: Colors.lightBlue,
+                  child: getCurrencySelector(),
+                ),
+              ],
+            );
+          } else if (snapshot.hasError) {
+            print(snapshot.error);
+            return Center(
+              child: Text(
+                'No more API requests left :(',
+                style: TextStyle(color: Colors.black),
               ),
-            ),
-          ),
-          Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: getCurrencySelector(),
-          ),
-        ],
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
@@ -75,14 +140,14 @@ class _PriceScreenState extends State<PriceScreen> {
       onChanged: (value) {
         setState(() {
           selectedValue = value;
+          // coinData.getCoinDataFor(selectedValue);
         });
       },
     );
   }
 
   CupertinoPicker iosPicker() {
-    List<Widget> currencies =
-        currenciesList.map((currency) => Text(currency)).toList();
+    List<Widget> currencies = currenciesList.map((currency) => Text(currency)).toList();
 
     return CupertinoPicker(
       backgroundColor: Colors.lightBlue,
@@ -92,5 +157,31 @@ class _PriceScreenState extends State<PriceScreen> {
       },
       children: currencies,
     );
+  }
+
+  List<Widget> cardsWithPadding(List<Map<String, double>> exchangeData) {
+    return exchangeData.map((Map<String, double> data) {
+      return Padding(
+        padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+        child: Card(
+          color: Colors.lightBlueAccent,
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+            child: Text(
+              '1 ${exchangeData[0]} = ${data[selectedValue]} $selectedValue',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      );
+    }).toList();
   }
 }
